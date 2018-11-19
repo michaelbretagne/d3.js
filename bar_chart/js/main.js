10e1fcdcc24e99e200a71ed9dbcafaded06bff68
Michael <michael.bretagne@gmail.com>
2018-11-18 17:32:25 -0800

    Make chart dynamic using d3 update pattern

--- bar_chart/js/main.js
+++ bar_chart/js/main.js
@@ -1,8 +1,13 @@
 // Bar chart
 
 var margin = { top: 10, right: 20, bottom: 150, left: 100 };
+
 var width = 600 - margin.left - margin.right;
 var height = 400 - margin.left - margin.right;
+
+var flag = true;
+
+var t = d3.transition().duration(750);
 
 var g = d3
   .select("#chart-area")
@@ -11,6 +16,23 @@ var g = d3
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+var xAxisGroup = g
+  .append("g")
+  .attr("class", "x axis")
+  .attr("transform", `translate(0, ${height})`);
+
+var yAxisGroup = g.append("g").attr("class", "y axis");
+
+// X scale
+var x = d3
+  .scaleBand()
+  .range([0, width])
+  .paddingInner(0.3)
+  .paddingOuter(0.3);
+
+// Y scale
+var y = d3.scaleLinear().range([height, 0]);
 
 // x label
 g.append("text")
@@ -21,7 +43,8 @@ g.append("text")
   .text("Month");
 
 // y label
-g.append("text")
+var yLabel = g
+  .append("text")
   .attr("x", -(height / 2))
   .attr("y", -60)
   .attr("font-size", "20px")
@@ -29,52 +52,72 @@ g.append("text")
   .attr("transform", "rotate(-90)")
   .text("Revenue");
 
+// Get the data from json file
 d3.json("data/revenues.json")
   .then(data => {
     data.forEach(d => {
       d.revenue = +d.revenue;
       d.profit = +d.profit;
     });
-    console.table(data);
 
-    var x = d3
-      .scaleBand()
-      .domain(data.map(d => d.month))
-      .range([0, width])
-      .paddingInner(0.3)
-      .paddingOuter(0.3);
+    d3.interval(() => {
+      var newData = flag ? data : data.slice(1);
 
-    var y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, d => d.revenue)])
-      .range([height, 0]);
+      update(newData);
+      flag = !flag;
+    }, 1000);
 
-    // x axis
-    var xAxisCall = d3.axisBottom(x);
-
-    g.append("g")
-      .attr("class", "bottom axis")
-      .attr("transform", `translate(0, ${height})`)
-      .call(xAxisCall);
-
-    // y axis
-    var yAxisCall = d3.axisLeft(y);
-
-    g.append("g")
-      .attr("class", "left axis")
-      .call(yAxisCall);
-
-    var rects = g.selectAll("rect").data(data);
-
-    rects
-      .enter()
-      .append("rect")
-      .attr("width", x.bandwidth)
-      .attr("height", d => height - y(d.revenue))
-      .attr("x", d => x(d.month))
-      .attr("y", d => y(d.revenue))
-      .attr("fill", "grey");
+    // Run the visualization for the 1st time
+    update(data);
   })
   .catch(error => {
     console.log(error);
   });
+
+var update = data => {
+  var value = flag ? "revenue" : "profit";
+
+  // Domains
+  x.domain(data.map(d => d.month));
+  y.domain([0, d3.max(data, d => d[value])]);
+
+  // X axis
+  var xAxisCall = d3.axisBottom(x);
+  xAxisGroup.transition(t).call(xAxisCall);
+
+  // Y axis
+  var yAxisCall = d3.axisLeft(y);
+  yAxisGroup.transition(t).call(yAxisCall);
+
+  // Join new data with old elements
+  var rects = g.selectAll("rect").data(data, d => d.month);
+
+  // Exit old elements not present in new data
+  rects
+    .exit()
+    .attr("fill", "red")
+    .transition(t)
+    .attr("y", y(0))
+    .attr("height", 0)
+    .remove();
+
+  // Enter new elements present in new data...
+  rects
+    .enter()
+    .append("rect")
+    .attr("fill", "grey")
+    .attr("y", y(0))
+    .attr("height", 0)
+    .attr("x", d => x(d.month))
+    .attr("width", x.bandwidth)
+    // And update old elements present in new data
+    .merge(rects)
+    .transition(t)
+    .attr("x", d => x(d.month))
+    .attr("width", x.bandwidth)
+    .attr("y", d => y(d[value]))
+    .attr("height", d => height - y(d[value]));
+
+  var label = flag ? "Revenue" : "Profit";
+  yLabel.text(label);
+};
